78248ed81e23dbb2f47e43756bf95505c84a0e5e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-12 19:43:46 +0900

    AssetBundleをビルドするためのクラスをとりあえず定義 ※今のところメニューを呼び出したらログ出力するだけ

---
+++ Assets/Editor/Scripts/AssetBundleBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleBuilder
+{
+    /// <summary>
+    /// AssetBundleのビルド
+    /// </summary>
+    [MenuItem("AssetBundle/Build")]
+    public static void Build()
+    {
+        Debug.Log("Build AssetBundle");
+    }
+}
