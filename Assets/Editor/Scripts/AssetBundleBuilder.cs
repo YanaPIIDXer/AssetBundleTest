b44d5b8c5cacd087979a8d73e017b95b814958c0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-12 20:02:09 +0900

    AssetBundleの名前を指定する処理を実装

--- Assets/Editor/Scripts/AssetBundleBuilder.cs
+++ Assets/Editor/Scripts/AssetBundleBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 /// <summary>
 /// AssetBundleビルドクラス
@@ -9,11 +10,35 @@ using UnityEditor;
 public class AssetBundleBuilder
 {
     /// <summary>
+    /// AssetBundleに突っ込むAssetを格納しているルートディレクトリ
+    /// </summary>
+    private static readonly string AssetBundleRootDirectory = "Assets/Editor/Assets/Bundle/";
+
+    /// <summary>
     /// AssetBundleのビルド
     /// </summary>
     [MenuItem("AssetBundle/Build")]
     public static void Build()
     {
-        Debug.Log("Build AssetBundle");
+        // AssetBundleにするディレクトリを列挙
+        string[] BundleDirs = Directory.GetFileSystemEntries(AssetBundleRootDirectory);
+        foreach (var BundleDir in BundleDirs)
+        {
+            if (BundleDir.IndexOf(".meta") >= 0) { continue; }
+            var BundleName = Path.GetFileNameWithoutExtension(BundleDir);
+            // ディレクトリ内のAssetを列挙してAssetBudnleの名前を指定
+            var Assets = Directory.GetFileSystemEntries(BundleDir);
+            foreach (var Asset in Assets)
+            {
+                if (Asset.IndexOf(".meta") >= 0) { continue; }
+                AssetImporter importer = AssetImporter.GetAtPath(Asset);
+                if (importer == null)
+                {
+                    Debug.LogError("AssetImporter.GetAtPath Failed. Asset:" + Asset);
+                    continue;
+                }
+                importer.SetAssetBundleNameAndVariant(BundleName, "");
+            }
+        }
     }
 }
